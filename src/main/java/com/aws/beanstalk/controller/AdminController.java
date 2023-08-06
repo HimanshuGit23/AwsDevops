caf6d6cee71a206de0c3323b85f182056335f4ec
HimanshuGit23 <marchaws23@gmail.com>
2023-08-06 19:43:45 +0530

    Post method added

--- src/main/java/com/aws/beanstalk/controller/AdminController.java
+++ src/main/java/com/aws/beanstalk/controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.aws.beanstalk.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,12 @@ public class AdminController {
 	@GetMapping("/getMessage")
 	public String getMessage() {
 		return "Welcome to Admin Controller";
+	}
+	
+	@PostMapping("/save/{id}")
+	public String saveAdmin(@PathVariable String id){
+		return "Admin saved....";
+		
 	}
 
 }
